c15f6725988c5bba29e80ab052223265f8621f80
itsakshay-git <110621476+itsakshay-git@users.noreply.github.com>
2025-08-19 19:55:05 +0530

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const corsOptions = {
         const allowedOrigins = [
             "http://localhost:5173",
             "https://manthetic-admin.vercel.app",
+            "https://manthetic-storefront.vercel.app",
             "https://manthetic.onrender.com"
         ];
 
