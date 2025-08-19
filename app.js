03d7c9ebb8f160f89a9f86fae503ddddef5cd472
itsakshay-git <110621476+itsakshay-git@users.noreply.github.com>
2025-08-19 19:08:42 +0530

    added cors config

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://manthetic.onrender.com",
+    ],
+    credentials: true
+}))
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
